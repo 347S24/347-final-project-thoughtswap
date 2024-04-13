c9234b0aded28a70f5e34101640f68ab81017588
TalishaMurray <murraytl@dukes.jmu.edu>
2024-04-13 15:22:07 -0400

    start discussion and quick swap

--- thoughtSwap/discussion/static/js/script.js
+++ thoughtSwap/discussion/static/js/script.js
@@ -1,6 +1,10 @@
 // When the user clicks on <div>, open the popup
 
-function Swaps() {
-  const popup = document.getElementById("quickSwap");
+function quickSwap() {
+  const popup = document.getElementById("swaps");
+  popup.classList.toggle("show");
+}
+function Popup() {
+  const popup = document.getElementById("pop");
   popup.classList.toggle("show");
 }
