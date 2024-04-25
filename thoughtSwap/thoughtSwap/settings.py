9e01ab3f7300f00f497909f2a40e8a92cceb92f0
Vincent Phan <phanvm@dukes.jmu.edu>
2024-04-25 10:28:04 -0400

    Change to file structure

--- thoughtSwap/thoughtSwap/settings.py
+++ thoughtSwap/thoughtSwap/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = "facilitator-profile"
+LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "index"  
 
 # daphne ASGI application
