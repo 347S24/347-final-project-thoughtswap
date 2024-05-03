dafb53783916820a189b5f20291865a9e030810d
jtminster4 <minstejt@dukes.jmu.edu>
2024-05-03 16:54:39 -0400

    Swappinggit add .

--- discussion/static/js/socket.js
+++ discussion/static/js/socket.js
@@ -1,9 +1,10 @@
 console.log("socket.js loaded");
 
-function connectChat(code) {
+function connectChat(code, user) {
     return new Promise((resolve, reject) => {
         console.log('Connecting to chat...', code);
         let host = window.location.host
+        console.log('user is:', user)
         chatSocket = new WebSocket(
             'ws://' + host +
             '/ws/discussion/' + code + '/'
@@ -12,7 +13,7 @@ function connectChat(code) {
         console.log('chatsocket', chatSocket)
 
         chatSocket.onopen = function (e) {
-            resolve();
+            resolve(chatSocket);
         };
 
         chatSocket.onerror = function (e) {
@@ -21,39 +22,41 @@ function connectChat(code) {
         chatSocket.onmessage = function (e) {
             const data = JSON.parse(e.data);
             console.log('data', data)
+            console.log('data', data.swap)
 
             if (data.swap) {
-                console.log('swap view');
-                console.log('prompt', prompt)
-                // let container = document.querySelector('.swap-display');
-                // console.log(container)
-                // container.classList.toggle('show-swap-response');
-                let container = document.querySelector('.swap-display');
-                if (container) {
-                    console.log(container)
+                if (data.swap[author]) {
+                    console.log('swap[user]', data.swap[author])
+                    let container = document.querySelector('.swap-display');
+                    let response_container = document.querySelector('.participant-response');
+                    let div = document.querySelector('#distributed-thought');
+                    div.innerHTML = data.swap[author];
                     container.classList.toggle('show-swap-response');
-                } else {
-                    console.log('.swap-display element not found');
+                    response_container.classList.toggle('show-swap-response');
                 }
 
-                let p = document.createElement('p');
-                p.textContent = data.prompt.trim();
-                document.querySelector('.prompt-display').innerHTML = p.textContent;
-                document.querySelector('#prompt-message-input').value = data.prompt;
+            } else if (data.save == false && data.prompt == '') {
+                let parent = document.querySelector('.response');
+                let children = document.querySelectorAll('.response-item');
+                children.forEach(function (child) {
+                    if (child.textContent.trim() == data.message.trim()) {
+                        parent.removeChild(child);
+                    }
+                });
             } else {
                 if (data.message) {
                     console.log('message', data.message)
                     let div = document.createElement('div');
                     div.className = 'response-item';
                     div.innerHTML = data.message;
-                    
+
                     // if no url, do not display delete button
                     if (deleteGroupUrl) {
                         console.log('fac view delete element')
                         let img = document.createElement('img');
                         img.src = "/static//media/trash-svgrepo-com.svg";
                         img.alt = 'delete thought';
-                        
+
                         let btn = document.createElement('button');
                         btn.className = 'ud-button delete-thought';
                         // console.log(deleteGroupUrl)
@@ -61,7 +64,7 @@ function connectChat(code) {
                         btn.type = "button"
                         btn.onclick = deleteThought;
                         btn.appendChild(img);
-        
+
                         let tip_container = document.createElement('div');
                         tip_container.className = 'tip-container';
                         tip_container.appendChild(btn);
@@ -173,9 +176,12 @@ function deleteSwapMessage(message, prompt, id, code, author) {
 }
 
 function deleteThought(e) {
+    // let parent_board = e.target.parentElement.parentElement;
+    // let item = e.target.parentElement.parentElement.parentElement;
     const thought = e.target.parentElement.parentElement.parentElement.textContent.trim();
     const popup = document.getElementById("delete-popup");
     const span = document.getElementById("thought");
     span.innerText = thought;
     popup.classList.toggle("show-delete");
-  }
+    // parent_board.removeChild(item);
+}
